0a5a4dabe68bad0ef485b83063d799a040543087
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2022-10-14 10:20:51 -0400

    added some temp changes

--- main.py
+++ main.py
@@ -1,2 +1,18 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 import webbrowser
+import time
+
+
+gameCode = ""
+gameUrl = "https://www.gimkit.com/join?gc="
+gameCode = input('Enter game code here: ')
+gameUrl = gameUrl + str(gameCode)
+
+driver = webdriver.Chrome()
+
+driver.get(gameUrl)
+
+buttonFish = driver.find_elements(By.ID, "insert Button ID here")
+
+buttonFish.click()
