23dcf1201730b205e1c77f0e709e2e43cad0ca23
qDread-dev <58754377+qDread-dev@users.noreply.github.com>
2022-10-14 10:42:45 -0400

    restructuring project

--- main.py
+++ main.py
@@ -3,22 +3,5 @@ from selenium.webdriver.common.by import By
 import webbrowser
 import time
 
-#todo:
-#Add option to pause loop
-#alert when bait empty
-
-
-
-gameCode = ""
-gameUrl = "https://www.gimkit.com/join?gc="
-gameCode = input('Enter game code here: ')
-gameUrl = gameUrl + str(gameCode)
-
-driver = webdriver.Chrome()
-
-driver.get(gameUrl)
-
-buttonFish = driver.find_elements(By.ID, "insert Button ID here")
-
-while True:
-    buttonFish.click()
+#always attempt to fish
+#
